7f4c27fc1f8e4add2f28f551ce6e759b902383f7
nst96 <nst96@192.168.0.6>
2022-08-04 14:52:36 +0530

    panu= hello

---
+++ src/pranita1/fgrg.java
@@ -0,0 +1,9 @@
+package pranita1;
+
+public class fgrg 
+{
+public static void main(String [] args)
+{
+	System.out.println("happy journey");
+}
+}
---
+++ src/pranita1/xyz.java
@@ -0,0 +1,11 @@
+package pranita1;
+
+public class xyz
+{
+public static void main(String [] args)
+{
+	System.out.println("byebye");
+}
+	
+}
+
